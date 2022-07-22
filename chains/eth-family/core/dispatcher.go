a2b9deebc7e01b4040082ed9596680a52fa10e78
Benja Heart <510036+visopsys@users.noreply.github.com>
2022-07-22 09:11:06 -0700

    Use use multiple clients for ETH dispatcher (#35)
    * Make multiple clients for eth dispatcher
    
    * Fix test
    
    * Return nil error if a transaction has been included in the blockchain when submitting
    
    * Print the RPC that dispatcher is using
    
    * Correct healthy statementgst
    
    * Correct log
    
    * Continue sending tx when error
    
    * Shuffle healthy rpcs
    
    Co-authored-by: Benja Heart <visopsys@users.noreply.github.com>

--- chains/eth-family/core/dispatcher.go
+++ chains/eth-family/core/dispatcher.go
@@ -3,7 +3,9 @@ package core
 import (
 	"context"
 	"fmt"
+	"math/rand"
 
+	"github.com/ethereum/go-ethereum/common"
 	eTypes "github.com/ethereum/go-ethereum/core/types"
 
 	"github.com/ethereum/go-ethereum/crypto"
@@ -15,25 +17,40 @@ import (
 )
 
 type EthDispatcher struct {
-	chain  string
-	rpcs   []string
-	client *ethclient.Client
+	chain   string
+	rpcs    []string
+	clients []*ethclient.Client
+	healthy []bool
 }
 
 func NewEhtDispatcher(chain string, rpcs []string) chains.Dispatcher {
 	return &EthDispatcher{
-		chain: chain,
-		rpcs:  rpcs,
+		chain:   chain,
+		rpcs:    rpcs,
+		healthy: make([]bool, len(rpcs)),
+		clients: make([]*ethclient.Client, len(rpcs)),
 	}
 }
 
 func (d *EthDispatcher) Start() {
+	d.dial()
+}
+
+func (d *EthDispatcher) dial() {
 	var err error
-	d.client, err = ethclient.Dial(d.rpcs[0])
-	if err != nil {
-		log.Error("Cannot dial chain", d.chain, "at endpoint", d.rpcs[0])
-		// TODO: Add retry mechanism here.
-		return
+	for i := range d.rpcs {
+		if d.clients[i] != nil {
+			d.clients[i].Close()
+		}
+		d.clients[i], err = ethclient.Dial(d.rpcs[i])
+		if err != nil {
+			log.Error("Cannot dial chain", d.chain, "at endpoint", d.rpcs[i])
+			d.healthy[i] = false
+			log.Infof("RPC %s is NOT healthy", d.rpcs[i])
+		} else {
+			d.healthy[i] = true
+			log.Infof("RPC %s is healthy", d.rpcs[i])
+		}
 	}
 }
 
@@ -59,27 +76,88 @@ func (d *EthDispatcher) Dispatch(request *types.DispatchedTxRequest) *types.Disp
 		log.Info("Deploying address = ", addr, " for chain ", request.Chain)
 	}
 
-	if err := d.client.SendTransaction(context.Background(), tx); err != nil {
-		// It is possible that another node has deployed the same transaction. We check if the tx has
-		// been included into the blockchain or not.
-		_, _, err2 := d.client.TransactionByHash(context.Background(), tx.Hash())
-		if err2 != nil {
-			log.Error("cannot dispatch tx, from = ", from, " chain = ", request.Chain)
-			log.Error("cannot dispatch tx, err = ", err)
-			log.Error("cannot dispatch tx, err2 = ", err2)
-			return types.NewDispatchTxError(err)
+	err = d.tryDispatchTx(tx, request.Chain, from)
+	if err != nil {
+		// Try to connect again.
+		d.dial()
+		err = d.tryDispatchTx(tx, request.Chain, from) // try second time
+	}
+
+	if err == nil {
+		log.Verbose("Tx is dispatched successfully for chain ", request.Chain, " from ", from,
+			"txHash =", tx.Hash())
+		return &types.DispatchedTxResult{
+			Success:                 true,
+			DeployedAddr:            addr,
+			Chain:                   request.Chain,
+			TxHash:                  request.TxHash,
+			IsEthContractDeployment: request.IsEthContractDeployment,
+		}
+	}
+
+	return types.NewDispatchTxError(err)
+}
+
+func (d *EthDispatcher) tryDispatchTx(tx *eTypes.Transaction, chain string, from common.Address) error {
+	// Shuffle rpcs so that we will use different healthy rpc
+	clients, healthy, rpcs := d.shuffle()
+
+	for i := range clients {
+		if !healthy[i] {
+			log.Verbose("%s is not healthy", rpcs[i])
+			continue
 		}
 
-		log.Info("The transaction has been deployed before. Tx hash = ", tx.Hash().String())
+		log.Verbose("Trying rpc ", rpcs[i])
+		client := clients[i]
+		if err := client.SendTransaction(context.Background(), tx); err != nil {
+			// It is possible that another node has deployed the same transaction. We check if the tx has
+			// been included into the blockchain or not.
+			_, _, err2 := client.TransactionByHash(context.Background(), tx.Hash())
+			if err2 != nil {
+				log.Error("cannot dispatch tx, from = ", from, " chain = ", chain)
+				log.Error("cannot dispatch tx, err = ", err)
+				log.Error("cannot dispatch tx, err2 = ", err2)
+
+				d.healthy[i] = false
+				continue
+			}
+
+			log.Info("The transaction has been deployed before. Tx hash = ", tx.Hash().String())
+			return nil
+		}
 	}
 
-	log.Verbose("Tx is dispatched successfully for chain ", request.Chain, " from ", from, "txHash =", tx.Hash())
+	return fmt.Errorf("cannot dispatch eth tx")
+}
 
-	return &types.DispatchedTxResult{
-		Success:                 true,
-		DeployedAddr:            addr,
-		Chain:                   request.Chain,
-		TxHash:                  request.TxHash,
-		IsEthContractDeployment: request.IsEthContractDeployment,
+func (d *EthDispatcher) shuffle() ([]*ethclient.Client, []bool, []string) {
+	n := len(d.clients)
+
+	clients := make([]*ethclient.Client, n)
+	healthy := make([]bool, n)
+	rpcs := make([]string, n)
+
+	copy(clients, d.clients)
+	copy(healthy, d.healthy)
+	copy(rpcs, d.rpcs)
+
+	for i := 0; i < 20; i++ {
+		x := rand.Intn(n)
+		y := rand.Intn(n)
+
+		tmpClient := clients[x]
+		clients[x] = clients[y]
+		clients[y] = tmpClient
+
+		tmpHealth := healthy[x]
+		healthy[x] = healthy[y]
+		healthy[y] = tmpHealth
+
+		tmpRpc := rpcs[x]
+		rpcs[x] = rpcs[y]
+		rpcs[y] = tmpRpc
 	}
+
+	return clients, healthy, rpcs
 }
