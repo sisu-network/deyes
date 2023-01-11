5df337d25b55609887e1aa85c8421127a9b5b1ce
Foxy <taikhoan759501@gmail.com>
2023-01-11 09:50:51 +0700

    fix: unit test

--- chains/lisk/crypto/keys_test.go
+++ chains/lisk/crypto/keys_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	passphraseHash = "6f433e3cd0df3d0fe423fb41e803538b811cfc72"
+	passphraseHash = "ba20a2df297ff5db79764c7b4e778e00eaa81b665b551447fae4fdcd64c81b76"
 	passphrase     = "camera accident escape cricket frog pony record occur broken inhale waste swing"
 	privateKey, _  = hex.DecodeString("ba20a2df297ff5db79764c7b4e778e00eaa81b665b551447fae4fdcd64c81b76f0321539a45078365c1a65944d010876c0efe45c0446101dacced7a2f29aa289")
 	publicKey, _   = hex.DecodeString("f0321539a45078365c1a65944d010876c0efe45c0446101dacced7a2f29aa289")
