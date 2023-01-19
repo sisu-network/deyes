d7c294abc9b853071642ab97f3e8277efe19968c
Benja Heart <visopsys@users.noreply.github.com>
2023-01-19 01:43:22 -0800

    Use proto for TransactionMessage test

--- chains/lisk/types/asset_test.go
+++ chains/lisk/types/asset_test.go
@@ -1,17 +1,19 @@
 package types
 
 import (
+	reflect "reflect"
 	"testing"
+	"unicode"
 
-	"github.com/near/borsh-go"
+	"github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/require"
 )
 
 func TestAsset_Serialize(t *testing.T) {
-	moduleId := uint32(100)
-	assetId := uint32(100)
+	moduleId := uint32(70)
+	assetId := uint32(80)
 	fee := uint64(100)
-	nonce := uint64(100)
+	nonce := uint64(90)
 
 	txMsg := TransactionMessage{
 		ModuleID:        &moduleId,
@@ -22,12 +24,28 @@ func TestAsset_Serialize(t *testing.T) {
 		SenderPublicKey: []byte{100}, // TODO: check if this is correct
 	}
 
-	bz, err := borsh.Serialize(txMsg)
+	bz, err := proto.Marshal(&txMsg)
 	require.Nil(t, err)
 
 	tx := &TransactionMessage{}
-	err = borsh.Deserialize(tx, bz)
+	err = proto.Unmarshal(bz, tx)
 	require.Nil(t, err)
 
-	require.Equal(t, &txMsg, tx)
+	// Use reflection to compare all public fields
+	types := reflect.TypeOf(*tx)
+	values1 := reflect.ValueOf(*tx)
+	values2 := reflect.ValueOf(txMsg)
+	for i := 0; i < values1.NumField(); i++ {
+		fieldName := types.Field(i).Name
+		if !unicode.IsUpper(rune(fieldName[0])) {
+			continue
+		}
+
+		f1 := values1.Field(i)
+		if f1.Kind() == reflect.Pointer {
+			require.Equal(t, f1.Elem().Interface(), values2.Field(i).Elem().Interface())
+		} else {
+			require.Equal(t, f1.Interface(), values2.Field(i).Interface())
+		}
+	}
 }
